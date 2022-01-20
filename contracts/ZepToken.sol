368496b6883763bf622610f61d95655ab3f10fba
blockedby <kasnis12@gmail.com>
2022-01-21 00:08:25 +0300

    cleaned for commnts, require of account != 0 fixed

--- contracts/ZepToken.sol
+++ contracts/ZepToken.sol
@@ -2,18 +2,6 @@
 
 pragma solidity ^0.8.0;
 
-// import "./Token.sol";
-// import "../../math/SafeMath.sol";
-
-/**
- * @title Standard ERC20 token
- *
- * @dev Implementation of the basic standard token.
- * https://github.com/ethereum/EIPs/blob/master/EIPS/eip-20.md
- * Originally based on code by FirstBlood: https://github.com/Firstbloodio/token/blob/master/smart_contract/FirstBloodToken.sol
- */
-
-
 interface IERC20 {
 
     function totalSupply() external view returns (uint256);
@@ -30,46 +18,24 @@ interface IERC20 {
 }
 
 contract ERC20 is IERC20 {
-//   using SafeMath for uint256;
-
   mapping (address => uint256) private _balances;
-
   mapping (address => mapping (address => uint256)) private _allowed;
-
   uint256 private _totalSupply;
 
-  /**
-  * @dev Total number of tokens in existence
-  */
+
   function totalSupply() public view override returns (uint256) {
     return _totalSupply;
   }
 
-  /**
-  * @dev Gets the balance of the specified address.
-  * @param owner The address to query the balance of.
-  * @return An uint256 representing the amount owned by the passed address.
-  */
   function balanceOf(address owner) public view override returns (uint256) {
     return _balances[owner];
   }
 
-  /**
-   * @dev Function to check the amount of tokens that an owner allowed to a spender.
-   * @param owner address The address which owns the funds.
-   * @param spender address The address which will spend the funds.
-   * @return A uint256 specifying the amount of tokens still available for the spender.
-   */
   function allowance(address owner,address spender) public view override returns (uint256)
   {
     return _allowed[owner][spender];
   }
 
-  /**
-  * @dev Transfer token for a specified address
-  * @param to The address to transfer to.
-  * @param value The amount to be transferred.
-  */
   function transfer(address to, uint256 value) public override returns (bool) {
     require(value <= _balances[msg.sender], "Balance less then value");
     require(to != address(0),"'To' can't be zero");
@@ -80,15 +46,6 @@ contract ERC20 is IERC20 {
     return true;
   }
 
-  /**
-   * @dev Approve the passed address to spend the specified amount of tokens on behalf of msg.sender.
-   * Beware that changing an allowance with this method brings the risk that someone may use both the old
-   * and the new allowance by unfortunate transaction ordering. One possible solution to mitigate this
-   * race condition is to first reduce the spender's allowance to 0 and set the desired value afterwards:
-   * https://github.com/ethereum/EIPs/issues/20#issuecomment-263524729
-   * @param spender The address which will spend the funds.
-   * @param value The amount of tokens to be spent.
-   */
   function approve(address spender, uint256 value) public override returns (bool) {
     require(spender != address(0),"'Spender' can't be zero");
 
@@ -97,12 +54,6 @@ contract ERC20 is IERC20 {
     return true;
   }
 
-  /**
-   * @dev Transfer tokens from one address to another
-   * @param from address The address which you want to send tokens from
-   * @param to address The address which you want to transfer to
-   * @param value uint256 the amount of tokens to be transferred
-   */
   function transferFrom(address from, address to, uint256 value) public override returns (bool){
     require(value <= _balances[from],"Balance less then value");
     require(value <= _allowed[from][msg.sender],"Such amount is not allowed");
@@ -115,15 +66,6 @@ contract ERC20 is IERC20 {
     return true;
   }
 
-  /**
-   * @dev Increase the amount of tokens that an owner allowed to a spender.
-   * approve should be called when allowed_[_spender] == 0. To increment
-   * allowed value is better to use this function to avoid 2 calls (and wait until
-   * the first transaction is mined)
-   * From MonolithDAO Token.sol
-   * @param spender The address which will spend the funds.
-   * @param addedValue The amount of tokens to increase the allowance by.
-   */
   function increaseAllowance(address spender,uint256 addedValue) public returns (bool){
     require(spender != address(0),"'Spender' can't be zero");
 
@@ -132,15 +74,6 @@ contract ERC20 is IERC20 {
     return true;
   }
 
-  /**
-   * @dev Decrease the amount of tokens that an owner allowed to a spender.
-   * approve should be called when allowed_[_spender] == 0. To decrement
-   * allowed value is better to use this function to avoid 2 calls (and wait until
-   * the first transaction is mined)
-   * From MonolithDAO Token.sol
-   * @param spender The address which will spend the funds.
-   * @param subtractedValue The amount of tokens to decrease the allowance by.
-   */
   function decreaseAllowance(
     address spender,
     uint256 subtractedValue
@@ -155,28 +88,15 @@ contract ERC20 is IERC20 {
     return true;
   }
 
-  /**
-   * @dev Internal function that mints an amount of the token and assigns it to
-   * an account. This encapsulates the modification of balances such that the
-   * proper events are emitted.
-   * @param account The account that will receive the created tokens.
-   * @param amount The amount that will be created.
-   */
   function _mint(address account, uint256 amount) internal {
-    // require(account != 0); TODO FIX
+    require(account != address(0),"Account can't be zero");
     _totalSupply += amount;
     _balances[account] += amount;
     emit Transfer(address(0), account, amount);
   }
 
-  /**
-   * @dev Internal function that burns an amount of the token of a given
-   * account.
-   * @param account The account whose tokens will be burnt.
-   * @param amount The amount that will be burnt.
-   */
   function _burn(address account, uint256 amount) internal {
-    // require(account != 0); TODO FIX
+    require(account != address(0),"Account can't be zero");
     require(amount <= _balances[account],"You don't have such amount");
 
     _totalSupply -= amount;
@@ -184,18 +104,9 @@ contract ERC20 is IERC20 {
     emit Transfer(account, address(0), amount);
   }
 
-  /**
-   * @dev Internal function that burns an amount of the token of a given
-   * account, deducting from the sender's allowance for said account. Uses the
-   * internal burn function.
-   * @param account The account whose tokens will be burnt.
-   * @param amount The amount that will be burnt.
-   */
   function _burnFrom(address account, uint256 amount) internal {
     require(amount <= _allowed[account][msg.sender],"Account doesn't own such amount");
 
-    // Should https://github.com/OpenZeppelin/zeppelin-solidity/issues/707 be accepted,
-    // this function needs to emit an event with the updated approval.
     _allowed[account][msg.sender] -= amount;
     _burn(account, amount);
   }
